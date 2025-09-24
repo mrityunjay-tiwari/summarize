bb760b7a0ef86810a49d598667dafeb41ff41d3c
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2025-09-24 23:54:03 +0530

    eight commit

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -1,23 +1,25 @@
-import { Document } from 'langchain/document'
-import pdf from 'pdf-parse';
+// import { Document } from 'langchain/document'
+// import pdf from 'pdf-parse';
+
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf'
 
 export async function fetchAndExtractPDFText(fileUrl: string) {
     const response = await fetch(fileUrl);
 
-    const arrayBuffer = await response.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
+    // const arrayBuffer = await response.arrayBuffer();
+    // const buffer = Buffer.from(arrayBuffer);
 
-    const data = await pdf(buffer);
+    // const data = await pdf(buffer);
 
-    // const blob = await response.blob();
+    const blob = await response.blob();
 
-    // const arrayBuffer = await blob.arrayBuffer();
+    const arrayBuffer = await blob.arrayBuffer();
 
-    // const loader = new PDFLoader(new Blob([arrayBuffer]));
+    const loader = new PDFLoader(new Blob([arrayBuffer]));
 
-    // const docs = await loader.load();
+    const docs = await loader.load();
 
-    const docs = [new Document({ pageContent: data.text })];
+    // const docs = [new Document({ pageContent: data.text })];
 
     // combine
     return docs.map((doc) => doc.pageContent).join('\n')
