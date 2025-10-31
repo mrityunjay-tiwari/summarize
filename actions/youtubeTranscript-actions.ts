f50bbf89bd618a2bbe59caf20c3d7ff3dac8b264
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2025-10-31 10:52:16 +0530

    yotube_video_title_update

--- actions/youtubeTranscript-actions.ts
+++ actions/youtubeTranscript-actions.ts
@@ -14,7 +14,7 @@ export async function youTubeTranscript(props : youTubeTranscriptProps) {
 
     const docs = await loader.load();
     const pageContent = docs[0].pageContent;
-    const videoTitle = docs[0].metadata.title
+    const videoTitle = docs[0].metadata.title || "Untitled Video"
     console.log(docs);
     console.log('docs pagecontent : ',docs[0].pageContent);
     
