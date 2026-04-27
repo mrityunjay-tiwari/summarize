c388a8d219e7f9e73710f0c5a012000de0355506
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2026-04-27 11:19:51 +0530

    Untrack testing files

--- app/quiz/page.tsx
+++
@@ -1,9 +0,0 @@
-import { QuizCarousel } from "@/components/individual-project/quiz-tab/quiz-carousel";
-
-export default function QuizPage() {
-  return (
-    <div>
-      <QuizCarousel quizzes={[]} />
-    </div>
-  );
-}
